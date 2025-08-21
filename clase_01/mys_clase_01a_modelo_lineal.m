795ea9617926706f52989304ac685b305a5e0a39
gsalomone <gianfranco.s@gmail.com>
2025-08-21 15:49:12 -0300

    refactor: add plot for analytic function

--- clase_01/mys_clase_01a_modelo_lineal.m
+++ clase_01/mys_clase_01a_modelo_lineal.m
@@ -7,7 +7,7 @@ clear all;
 
 % U0:      acción externa  [m^3/s]
 % tfinal:  tiempo final    [s]
-% ho:      nivel inicial   [m]
+% h0:      nivel inicial   [m]
 
 % k:       cte de prop     [m^2/s] (se deduce!)
 % C:       sección tanque  [m^2]
@@ -20,7 +20,22 @@ h0 = 4;
 % Constantes del sistema:
 k = 0.3;
 C = 1;
+
+% ---------------- Simulación con expresión analítica ----------------
+t_analitica = 0:.1:50;
+
+% Expresión analítica de la solución de la ecuación diferencial.
+h_analitica = U0/k + (h0 - U0/k) * exp(-k*t_analitica/C);
+
+figure(1);
+plot(t_analitica, h_analitica);
+title('Respuesta de h(t) (solución analítica)');
+xlabel('Tiempo (s)');
+ylabel('Nivel del tanque (m)');
  
+grid;
+
+% ---------------- Simulación numérica ----------------
 % Se define la ecuación diferencial
 % primero la var indep, luego la dependiente
 dh = @(t,h) -k*h/C + U0/C  % Es una función paramétrica
@@ -34,8 +49,9 @@ tspan = [0, tfinal]
 [t, h] = ode23(dh, tspan, h0);
 
 % Gráfica de la función y, una vez resuelta la ecuación diferencial:
+figure(2);
 plot(t, h);
-title('Respuesta de h(t)');
+title('Respuesta de h(t) (solución numérica)');
 xlabel('Tiempo (s)');
 ylabel('Nivel del tanque (m)');
  
